bbe60eaed20303c5c1473d7799f5936f184c031c
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2024-08-13 15:12:52 +0100

    Update app.py
    That doesn’t work I guess

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from urllib.parse import unquote
 app = Flask(__name__)
-@app.route("")
-def hello():
-  return "To evaluate code, please go to, for example, https://safe-exec.onrender.com/1+1<br/>That will give you the value of 1+1, which is 2."
 @app.route("/<code>")
 def execute(code):
   try:
