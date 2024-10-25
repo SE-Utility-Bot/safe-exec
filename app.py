8e002367810ef0c0899ff9fbbc686aa07e49b4e2
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2024-10-25 10:48:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,7 @@ app = Flask(__name__)
 def execute(code):
   try:
     f = open("code.py", "w")
-    f.write(f"""try:
-    \tprint({unquote(code)})
-    except BaseException as e:
-    \tprint(repr(e))""")
+    f.write(f"try:\n\tprint({unquote(code)})\nexcept BaseException as e:\n\tprint(repr(e))")
     f.close()
     carr = ["timeout", "-s", "SIGKILL", "10s", "python3", "code.py"]
     return subprocess.check_output(carr).decode("utf-8").replace("\n", "")
